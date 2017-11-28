36bdc1a27d728b16dca8f4a8f49893e030b2ab54
lucas hamani <hamanilucas@gmail.com>
2017-11-28 13:50:19 +0100

    rails task manager refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :tasks
   root "tasks#index"
   # all
-  get "tasks", to: "tasks#index"
+  # get "tasks", to: "tasks#index"
 
   # create
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
 
   # one
-  get "tasks/:id", to: "tasks#show", as: "task"
+  # get "tasks/:id", to: "tasks#show", as: "task"
 
   # update
-  get "tasks/:id/edit", to: "tasks#edit", as: "task_edit"
-  patch "tasks/:id", to: "tasks#update"
+  # get "tasks/:id/edit", to: "tasks#edit", as: "task_edit"
+  # patch "tasks/:id", to: "tasks#update"
 
-  # delete
-  delete "tasks/:id", to: "tasks#destroy"
+  # # delete
+  # delete "tasks/:id", to: "tasks#destroy"
 end
