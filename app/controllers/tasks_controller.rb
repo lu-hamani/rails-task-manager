809eca021318087a6f661877c5755cdba3adde2f
lucas hamani <hamanilucas@gmail.com>
2017-11-28 13:14:04 +0100

    task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :user)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  task = Task.new(name: Faker::Witcher.quote, description: Faker::Witcher.quote, user: Faker::Superhero.name)
+  task.save
+  puts "Seed done"
+end
